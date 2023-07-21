8ae816d10a29a83b8a0f22f0db95299097752c0f
lawnguyen27 <nguyenbanhattai27@gmail.com>
2023-07-21 20:39:19 +0700

    a

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ viewEngine(app);
 //config web routes
 webRoutes(app);
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 8081;
 
 app.listen(port, () => {
     console.log("App is running at the port: " + port);
